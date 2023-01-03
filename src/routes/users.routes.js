dde01440ac197351acab40603ed15aca0d361614
Matheus Vinicius <matheussvini@outlook.com>
2023-01-03 10:41:19 -0300

    initial configs

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+// import { createUser,findById, login } from "../controllers/users.controllers.js";
+
+const router = Router();
+
+// router.post('/signup', createUser);
+// router.post('/signin', login);
+// router.get('/users/me', authValidation , findById);
+
+export default router;
